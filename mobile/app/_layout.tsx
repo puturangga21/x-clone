20b7f2e3000dfc7fe8a91709560a58c02cc226e3
puturangga21 <puturangga21@gmail.com>
2025-07-03 16:25:05 +0800

    feat: enhance RootLayout with color scheme support and status bar configuration

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,11 +1,17 @@
-import { Stack } from 'expo-router';
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'react-native';
 import '../global.css';
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
     <ClerkProvider tokenCache={tokenCache}>
+      <StatusBar style={colorScheme === 'dark' ? 'dark' : 'light'} />
+
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       </Stack>
