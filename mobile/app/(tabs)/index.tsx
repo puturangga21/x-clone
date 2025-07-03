7f87327df5d9d8d136d0b1daa9c4de52f74766fc
puturangga21 <puturangga21@gmail.com>
2025-07-03 18:04:27 +0800

    fix

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
 import SignOutButton from '@/components/SignOutButton';
+import { useUserSync } from '@/hooks/useUserSync';
 import React from 'react';
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const IndexHome = () => {
+  useUserSync();
+
   return (
     <SafeAreaView className="flex-1">
       <Text>IndexHome</Text>
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,18 +3,22 @@ import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import '../global.css';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <StatusBar style={colorScheme === 'dark' ? 'dark' : 'light'} />
-
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      </Stack>
+      <QueryClientProvider client={queryClient}>
+        <StatusBar style={colorScheme === 'dark' ? 'dark' : 'light'} />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        </Stack>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 }
